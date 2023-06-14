9d66ae2390c59f18eece0f2f9581050ce6fca3f8
local user <36323477+ystgs@users.noreply.github.com>
2023-06-15 00:39:07 +0900

    hogehgoe

--- discord-bot/send-to-notion.js
+++ discord-bot/send-to-notion.js
@@ -1,9 +1,39 @@
 const axios = require("axios");
 
 const options = {
-  method: "GET",
-  url: "https://api.notion.com/v1/pages/page_id",
-  headers: { accept: "application/json", "Notion-Version": "2022-06-28" },
+  method: "POST",
+  url: "https://api.notion.com/v1/pages",
+  headers: {
+    accept: "application/json",
+    "Notion-Version": "2022-06-28",
+    "content-type": "application/json",
+    Authorization: "Bearer secret_ymvJN8h14QZom8wBJXNb0wvPZVtRFHHFp3sbEmqNc83",
+  },
+  data: {
+    parent: {
+      database_id: "ad9d405ef0574f6eaf061574d50d5178",
+    },
+    properties: {
+      Id: {
+        title: [
+          {
+            text: {
+              content: "ほげ",
+            },
+          },
+        ],
+      },
+      AuthorName: {
+        rich_text: [
+          {
+            text: {
+              content: "A dark green leafy vegetable",
+            },
+          },
+        ],
+      },
+    },
+  },
 };
 
 axios
