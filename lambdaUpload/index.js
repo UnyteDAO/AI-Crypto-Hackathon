cdfca8dee023e3605bcb8b7e681c9317041c80e7
gtyuki83 <yukiukiuki0803@gmail.com>
2023-06-14 23:45:26 +0900

    ✨lambdaの処理のみ分岐

---
+++ lambdaUpload/index.js
@@ -0,0 +1,139 @@
+// require('dotenv').config();
+exports.handler = async function (event, context) {
+// const handler = async () => {
+    const { ethers } = require("ethers");
+	const pinataSDK = require('@pinata/sdk');
+	const pinata = new pinataSDK({ pinataJWTKey: process.env.PINATA_JWT_KEY});
+
+	const createNFTImage = async (userid, username, icon, baseImage, borderImage, textColor) => {
+		const fs = require("fs");
+		const fetch = require("node-fetch");
+		const { Resvg } = require("@resvg/resvg-js");
+
+		const base = fs.readFileSync("./image/base.svg", "utf8");
+		let fontsize = "64";
+		username = username.substring(0, 24);
+		for (let i = 0; i < username.length; i++) {
+			if (username.charCodeAt(i) >= 256) {
+				username = username.substring(0, 24);
+				fontsize = "560";
+				break;
+			}
+		};
+
+		let svg = base.replaceAll("$username$", username);
+		svg = svg.replaceAll("$base-image$", baseImage);
+		svg = svg.replaceAll("$border-image$", borderImage);
+		svg = svg.replaceAll("$icon$", icon);
+		svg = svg.replaceAll("$text-color$", textColor);
+		svg = svg.replaceAll("$font-size$", fontsize);
+
+		//fs.writeFileSync(`./sbt-image/svg/${userid}.svg`, svg);
+
+		const opts = {
+			font: {
+				fontFiles: ['./example/SourceHanSerifCN-Light-subset.ttf'], // Load custom fonts.
+				loadSystemFonts: false, // It will be faster to disable loading system fonts.
+				defaultFontFamily: 'Source Han Serif CN Light',
+			},
+		};
+		const resvg = new Resvg(svg, opts);
+
+		const resolved = await Promise.all(
+			resvg.imagesToResolve().map(async (url) => {
+				const img = await fetch(url);
+				const buffer = await img.arrayBuffer();
+				return {
+					url,
+					buffer: Buffer.from(buffer),
+				};
+			}),
+		);
+
+		if (resolved.length > 0) {
+			for (const result of resolved) {
+				const { url, buffer } = result;
+				resvg.resolveImage(url, buffer);
+			}
+		}
+
+		const pngData = resvg.render();
+		const pngBuffer = pngData.asPng();
+		// 書き込み形式を変更
+		fs.writeFileSync(`/tmp/${userid}.png`, pngBuffer);
+		console.log(`created ${userid}.png`);
+		console.log(svg)
+		return(`/tmp/${userid}.png`);
+	};
+
+	// const event = {"body": {"status": "done","tasks": [{"team": {"name": "Super Team","id": "1234","avatarURL": "hoge"},"recipient": {"walletAddress":["0xdC56956cD5441348c758519AFb66a10D86cbe7b2"]},"requester": {"name":"Taro Yamada","id": "9876","avatarURL": "hoge"},"task": {"name": "JSON file creation","id": "8765"}}]}}
+    console.log(event)
+    const data = JSON.parse(event.body)
+	// const data = event.body
+	console.log(data)
+    const tasks = data.tasks
+
+	const status = data.status
+	const team = data.team
+	const requester = data.requester
+	console.log("status, ", status, "team, ", team, "requester, ", requester)
+
+    for (let index = 0; index < tasks.length; index++) {
+        const task = tasks[index];
+        const recipient = task.recipient
+        const taskContent = task.task
+        console.log(recipient,taskContent)
+		console.log(taskContent.name)
+
+		const path = await createNFTImage(requester.id, taskContent.name, "https://pbs.twimg.com/media/FyjEYVzacAEJwqD?format=jpg", "https://media.discordapp.net/attachments/1116624090241974364/1118224028734345266/20230613_NFT_A1.jpg", "https://pbs.twimg.com/media/FyjEYVzacAEJwqD?format=jpg", "white");
+
+		// データをipfsにアップロード
+		let hash = "";
+		// const options = {
+		// 	pinataMetadata: {
+		// 		name: `${taskContent.name}`
+		// 	},
+		// };
+		await pinata.pinFromFS(path).then((result) => {
+			//handle results here
+			console.log(result);
+			hash = result.IpfsHash;
+		}).catch((err) => {
+			//handle error here
+			console.log(err);
+			return;
+		});
+
+        // 秘密鍵からアカウントの作成
+        const provider = new ethers.providers.JsonRpcProvider("https://polygon-mumbai.g.alchemy.com/v2/OYM4nSdwayU_AlLiq50U7TFXnKqXXcuL");
+        const privateKey = process.env.PRIVATE_KEY;
+        const walletWithProvider = new ethers.Wallet(privateKey, provider);
+
+        // コントラクトのアドレスとABIを読み込む
+        const contractAddress = process.env.CONTRACT_ADDRESS;
+        const contractABI = require("./Hackathon.json");
+
+        // コントラクトのインスタンスを作成
+        const connectedContract = new ethers.Contract(contractAddress, contractABI.abi, walletWithProvider);
+
+        // タスク毎にNFTをmintする
+        const result = await connectedContract.mintBatch(
+            recipient.walletAddress,
+            `ipfs://${hash}`,
+            taskContent.id,
+            data.status
+        );
+        console.log(result)
+    }
+    
+    const response = {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Headers": "Content-Type"
+        },
+        body: JSON.stringify('success'),
+    };
+    return response;
+};
+
+// handler();
