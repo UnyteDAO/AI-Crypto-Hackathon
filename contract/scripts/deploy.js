794628e11aadf79e713969e1ba6db3172b29d5da
gtyuki83 <yukiukiuki0803@gmail.com>
2023-06-18 20:21:46 +0900

    🐛astar対応+デバッグ

--- contract/scripts/deploy.js
+++ contract/scripts/deploy.js
@@ -8,7 +8,7 @@ const main = async () => {
   const nftContract = await nftContractFactory.deploy();
   await nftContract.deployed();
   console.log("Contract deployed to:", nftContract.address);
-  let txn = await nftContract.mint("0x3a0bE810754f7f7D04fCA10E2C11E93ebb5BF19e","ipfs://Qma6NnmYjeVQy1c8jERyBboF1SvXpiiXmQuE8iNJsFeXYA","1","1");
+  let txn = await nftContract.mint("0x4C09EaeDb76fE46E44C69f5cE997faa7502e46F9","ipfs://Qma6NnmYjeVQy1c8jERyBboF1SvXpiiXmQuE8iNJsFeXYA","1","1");
   // Minting が仮想マイナーにより、承認されるのを待ちます。
   await txn.wait();
   console.log("Minted NFT");
@@ -16,7 +16,7 @@ const main = async () => {
 
   // バッチでNFTをミントする
   txn = await nftContract.mintBatch(
-    ["0xdC56956cD5441348c758519AFb66a10D86cbe7b2", "0x4C09EaeDb76fE46E44C69f5cE997faa7502e46F9"],
+    ["0x4C09EaeDb76fE46E44C69f5cE997faa7502e46F9", "0x4C09EaeDb76fE46E44C69f5cE997faa7502e46F9"],
     ["ipfs://Qma6NnmYjeVQy1c8jERyBboF1SvXpiiXmQuE8iNJsFeXYA", "ipfs://Qma6NnmYjeVQy1c8jERyBboF1SvXpiiXmQuE8iNJsFeXYA"],
     ["1", "2"],
     ["1", "2"]
